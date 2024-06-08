23ab70ee0a6ae5ce5159d532faf54e9c7db30a53
Xuesong Bai <15259109+xuesong-bai@users.noreply.github.com>
2024-06-07 23:44:06 -0700

    openbsd-compat/bsd-closefrom.c: don't close AFL forkserver fd

--- openbsd-compat/bsd-closefrom.c
+++ openbsd-compat/bsd-closefrom.c
@@ -56,6 +56,12 @@
 __unused static const char rcsid[] = "$Sudo: closefrom.c,v 1.11 2006/08/17 15:26:54 millert Exp $";
 #endif /* lint */
 
+static int
+is_afl_forkserver_fd(int fd)
+{
+	return fd == 198 || fd == 199;
+}
+
 #ifndef HAVE_FCNTL_CLOSEM
 /*
  * Close all file descriptors greater than or equal to lowfd.
@@ -79,11 +85,13 @@ closefrom_fallback(int lowfd)
 		maxfd = OPEN_MAX;
 
 	for (fd = lowfd; fd < maxfd; fd++)
-		(void) close((int) fd);
+		if (!is_afl_forkserver_fd(fd))
+			(void) close((int) fd);
 }
 #endif /* HAVE_FCNTL_CLOSEM */
 
 #ifdef HAVE_FCNTL_CLOSEM
+#error close() on AFL's FORKSRV_FD{, + 1}
 void
 closefrom(int lowfd)
 {
@@ -139,7 +147,7 @@ closefrom(int lowfd)
 	while ((dent = readdir(dirp)) != NULL) {
 	    fd = strtol(dent->d_name, &endp, 10);
 	    if (dent->d_name != endp && *endp == '\0' &&
-		fd >= 0 && fd < INT_MAX && fd >= lowfd && fd != dirfd(dirp))
+		fd >= 0 && fd < INT_MAX && fd >= lowfd && fd != dirfd(dirp) && !is_afl_forkserver_fd(fd))
 		(void) close((int) fd);
 	}
 	(void) closedir(dirp);
